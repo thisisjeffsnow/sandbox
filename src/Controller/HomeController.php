bc54fcaef77c617188ed99048bcc3cb572e0c11e
Jeff Snow <94205783+thisisjeffsnow@users.noreply.github.com>
2023-01-13 11:18:30 -0500

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace Sandbox\Controller;
+use Sandbox\Core\Request;
 
-class MainController
+class HomeController
 {
-    public function getMain()
+    public function getMain(Request $request): void
     {
         /* Call View class to display main page. */
         echo "Get Home";
